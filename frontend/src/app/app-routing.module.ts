8ecb9ae2b5be8f08bbf09c8e4fa088bc462d96e0
Nhlalala <nhlalala60@gmail.com>
2023-04-12 13:20:25 +0200

    added

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './components/navbar/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component:AppComponent},
+  {path: 'about',component:AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
