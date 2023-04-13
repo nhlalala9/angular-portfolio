e6e79dc38028cb4cd0a0a098db742050afbd99a4
Nhlalala <nhlalala60@gmail.com>
2023-04-13 12:29:13 +0200

    changes

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
-import { AboutComponent } from './components/navbar/about/about.component';
+
 
 const routes: Routes = [
-  {path: '',component:AppComponent},
-  {path: 'about',component:AboutComponent}
+  
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,21 +4,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/navbar/home/home.component';
-import { AboutComponent } from './components/navbar/about/about.component';
-import { ContactComponent } from './components/navbar/contact/contact.component';
-import { ProjectsComponent } from './components/navbar/projects/projects.component';
-import { SkillsComponent } from './components/navbar/skills/skills.component';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { SkillsComponent } from './components/skills/skills.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HomeComponent,
-    AboutComponent,
     ContactComponent,
     ProjectsComponent,
-    SkillsComponent
+    SkillsComponent,
+   
   ],
   imports: [
     BrowserModule,
