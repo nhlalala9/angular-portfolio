bce9abf9a4117a79f422050dfad962db91b2a76b
Nhlalala <nhlalala60@gmail.com>
2023-04-25 13:49:45 +0200

    background

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
 import { ContactComponent } from './components/contact/contact.component';
-import { CurriculumVitaeComponent } from './components/curriculum-vitae/curriculum-vitae.component';
+
 import { HomeComponent } from './components/home/home.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
@@ -15,7 +15,7 @@ const routes: Routes = [
   {path: 'contact',component:ContactComponent},
   { path: 'skills', component:SkillsComponent }, 
   { path: 'projects', component: ProjectsComponent }, 
-  { path: 'cv', component: CurriculumVitaeComponent }
+  
 
   
 ];
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HomeComponent } from './components/home/home.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { ProjectsComponent } from './components/projects/projects.component';
 import { SkillsComponent } from './components/skills/skills.component';
-import { CurriculumVitaeComponent } from './components/curriculum-vitae/curriculum-vitae.component';
 import { AboutComponent } from './components/about/about.component';
 
 
@@ -20,7 +19,6 @@ import { AboutComponent } from './components/about/about.component';
     ContactComponent,
     ProjectsComponent,
     SkillsComponent,
-    CurriculumVitaeComponent,
     AboutComponent,
    
   ],
